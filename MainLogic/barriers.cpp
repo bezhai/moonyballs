6c6b30e0e3346a328eb84d8e73ad5085b030ad7c
chin <cyjdtxz@gmail.com>
2019-07-02 16:58:11 +0800

    Barriers
    Barriers classes

---
+++ MainLogic/barriers.cpp
@@ -0,0 +1,182 @@
+// barriers.cpp -- barriers methods
+#include "pch.h"
+#include "barriers.h"
+#include <iostream>
+#include <ctime>
+#include <cmath>
+
+const double PI = atan(1.0) * 4;
+
+Barrier::~Barrier() {}
+
+Barrier::Barrier(double set_x, double set_y, int hp)
+{
+	this->x = set_x;
+	this->y = set_y;
+	this->hp = hp;
+}
+
+bool Barrier::IsCovered(const Barrier &br)
+{
+	double center_distance = sqrt(pow((this->x - br.x), 2.0) + pow((this->y - br.y), 2.0));
+	double sum_radius = this->calculate_radius + br.calculate_radius;
+	return sum_radius > center_distance;
+}
+
+CircleBarrier::CircleBarrier(double x, double y, double r, int hp) :Barrier(x, y, hp)
+{
+	this->radius = r;
+	this->SetCalculateRadius(r);
+}
+
+CircleBarrier::~CircleBarrier() {}
+
+RectangleBarrier::RectangleBarrier(double x, double y, double len, double wid, int hp, double rot) : Barrier(x, y, hp)
+{
+	this->length = len;
+	this->width = wid;
+	this->SetCalculateRadius(sqrt(pow(len, 2.0) + pow(wid, 2.0)));
+}
+
+RectangleBarrier::~RectangleBarrier() {}
+
+TriangleBarrier::TriangleBarrier(double x, double y, double len, int hp, double rot) :Barrier(x, y, hp)
+{
+	this->length = len;
+	this->rotation = rot;
+	this->SetCalculateRadius(len / SQRT_3);
+}
+
+TriangleBarrier::~TriangleBarrier() {}
+
+PentangoBarrier::PentangoBarrier(double x, double y, double r, int hp, double rot) :Barrier(x, y, hp)
+{
+	this->radius = r;
+	this->rotation = rot;
+	this->SetCalculateRadius(r);
+}
+
+PentangoBarrier::~PentangoBarrier() {}
+
+HexagonBarrier::HexagonBarrier(double x,double y, double r,int hp, double rot) :Barrier(x, y, hp)
+{
+	this->radius = r;
+	this->rotation = rot;
+	this->SetCalculateRadius(r);
+}
+
+HexagonBarrier::~HexagonBarrier() {}
+
+void TriangleBarrier::GetPos(double points[])
+{
+	double tempang;
+
+	double x = Barrier::GetX();
+	double y = Barrier::GetY();
+	double r = Barrier::GetCalculateRadius();
+	double rot = this->rotation;
+
+	tempang = rot + PI / 2;
+	points[0] = x + r*cos(tempang);
+	points[1] = y + r*sin(tempang);
+
+	tempang = rot + 7 * PI / 6;
+	points[2] = x+ r*cos(tempang);
+	points[3] = y + r*sin(tempang);
+
+	tempang = rot + 11 * PI / 6;
+	points[4] = x + r*cos(tempang);
+	points[5] = y + r*sin(tempang);
+
+}
+
+void RectangleBarrier::GetPos(double points[])
+{
+	double delta = atan(this->width / this->length);
+	double tempang;
+
+	double x = Barrier::GetX();
+	double y = Barrier::GetY();
+	double r = Barrier::GetCalculateRadius();
+	double rot = this->rotation;
+
+	tempang = delta + rot;
+	points[0] = x + r * cos(tempang);
+	points[1] = x + r * sin(tempang);
+	
+	tempang = PI - delta + rot;
+	points[2] = x + r * cos(tempang);
+	points[3] = y + r * sin(tempang);
+
+	tempang = PI + delta + rot;
+	points[4] = x + r * cos(tempang);
+	points[5] = y + r * sin(tempang);
+
+	tempang = 2 * PI - delta + rot;
+	points[6] = x + r * cos(tempang);
+	points[7] = y + r * sin(tempang);
+}
+
+void PentangoBarrier::GetPos(double points[])
+{
+	double tempang;
+
+	double x = Barrier::GetX();
+	double y = Barrier::GetY();
+	double r = Barrier::GetCalculateRadius();
+	double rot = this->rotation;
+
+	tempang = PI / 10 + rot;
+	points[0] = x + r * cos(tempang);
+	points[1] = x + r * sin(tempang);
+
+	tempang = PI / 2 + rot;
+	points[2] = x + r * cos(tempang);
+	points[3] = x + r * sin(tempang);
+
+	tempang = 9 * PI / 10 + rot;
+	points[4] = x + r * cos(tempang);
+	points[5] = x + r * sin(tempang);
+
+	tempang = 13 * PI / 10 + rot;
+	points[6] = x + r * cos(tempang);
+	points[7] = x + r * sin(tempang);
+
+	tempang = 17 * PI / 10 + rot;
+	points[8] = x + r * cos(tempang);
+	points[9] = x + r * sin(tempang);
+}
+
+void HexagonBarrier::GetPos(double points[])
+{
+	double tempang;
+
+	double x = Barrier::GetX();
+	double y = Barrier::GetY();
+	double r = Barrier::GetCalculateRadius();
+	double rot = this->rotation;
+
+	tempang = rot;
+	points[0] = x + r * cos(tempang);
+	points[1] = x + r * sin(tempang);
+
+	tempang = PI / 3 + rot;
+	points[2] = x + r * cos(tempang);
+	points[3] = x + r * sin(tempang);
+
+	tempang = 2 * PI / 3 + rot;
+	points[4] = x + r * cos(tempang);
+	points[5] = x + r * sin(tempang);
+
+	tempang = PI + rot;
+	points[6] = x + r * cos(tempang);
+	points[7] = x + r * sin(tempang);
+
+	tempang = 4 * PI / 3 + rot;
+	points[8] = x + r * cos(tempang);
+	points[9] = x + r * sin(tempang);
+
+	tempang = 5 * PI / 3 + rot;
+	points[10] = x + r * cos(tempang);
+	points[11] = x + r * sin(tempang);
+}
---
+++ MainLogic/barriers.h
@@ -0,0 +1,129 @@
+// barriers.h -- including barriers
+// Except circle and rectangle, all shapes are regular
+// Including Circle, Rectangle, Triangle, Pentagon, Hexagon
+/*
+	Base Class: Barrier
+	Derived Class: CircleBarrier、RectangleBarrier、TriangleBarrier、PentagonBarrier、HexagonBarrier
+*/
+#ifndef BARRIERS_H_
+#define BARRIERS_H_
+const double SQRT_3 = 1.732;
+enum MODE { CIRCLE, RECTANGLE, TRIANGLE, PENTAGON, HEXAGON };	// The judge standard
+class Barrier								// The base class of Barrier
+{
+public:
+	Barrier(double set_x, double set_y, int hp = 1);
+	virtual ~Barrier();
+
+	inline double GetX() { return this->x; }
+	inline double GetY() { return this->y; }
+	inline int GetHp() { return this->hp; }
+	inline double GetCalculateRadius() { return this->calculate_radius; }
+
+	inline void SetX(double set_x) { this->x = set_x; }
+	inline void SetY(double set_y) { this->y = set_y; }
+
+	inline void Damaged(int damage = 1) { this->hp -= damage; }
+	inline void SetHp(int set_hp) { this->hp = set_hp; }
+
+	virtual void SetCalculateRadius(double r) { this->calculate_radius = r; }
+	bool IsCovered(const Barrier &br);
+private:
+	double x;			// The position of centroid
+	double y;
+	int hp;				// The times of being delete
+	double calculate_radius;	// The radius of circumcircle
+};
+class CircleBarrier :public Barrier			// Circle
+{
+public:
+	CircleBarrier(double x, double y, double r, int hp = 1);
+	virtual ~CircleBarrier();
+
+	inline double GetRadius() { return this->radius; }
+
+	inline void SetRadius(double r) { this->radius = r; }
+	virtual void SetCalculateRadius(double r) { Barrier::SetCalculateRadius(r); }
+
+private:
+	double radius;
+ };
+class RectangleBarrier :public Barrier
+{
+public:
+	RectangleBarrier(double x, double y, double len, double wid, int hp = 1, double rot = 0.0);
+	virtual ~RectangleBarrier();
+
+	inline double GetLength() { return this->length; }
+	inline double GetWidth() { return this->width; }
+	inline double GetRot() { return this->rotation; }
+
+	inline void SetLength(double length) { this->length = length; }
+	inline void SetWidth(double width) { this->width = width; }
+	inline void SetRot(double rotation) { this->rotation = rotation; }
+
+	virtual void SetCalculateRadius(double r) { Barrier::SetCalculateRadius(r); }
+	virtual void GetPos(double points[]);
+
+private:
+	double length;
+	double width;
+	double rotation;
+};
+class TriangleBarrier :public Barrier
+{
+public:
+	TriangleBarrier(double x, double y, double len, int hp = 1, double rot = 0.0);
+	virtual ~TriangleBarrier();
+
+	inline double GetLength() { return this->length; }
+	inline double GetRot() { return this->rotation; }
+
+	inline void SetLength(double length) { this->length = length; }
+	inline void SetRot(double rotation) { this->rotation = rotation; }
+
+	virtual void SetCalculateRadius(double r) { Barrier::SetCalculateRadius(r); }
+	virtual void GetPos(double points[]);
+
+private:
+	double length;		// The length of the triangle's side
+	double rotation;
+};
+class PentangoBarrier :public Barrier
+{
+public:
+	PentangoBarrier(double x, double y, double r, int hp = 1, double rot = 0.0);
+	virtual ~PentangoBarrier();
+
+	inline double GetRadius() { return this->radius; }
+	inline double GetRot() { return this->rotation; }
+	
+	inline void SetLength(double radius) { this->radius = radius; }
+	inline void SetRot(double rotation) { this->rotation = rotation; }
+	
+	virtual void SetCalculateRadius(double r) { Barrier::SetCalculateRadius(r); }
+	virtual void GetPos(double points[]);
+private:
+	double radius;		// The distance between centroid and a vertex
+	double rotation;
+};
+class HexagonBarrier :public Barrier
+{
+public:
+	HexagonBarrier(double x, double y, double r, int hp = 1, double rot = 0.0);
+	virtual ~HexagonBarrier();
+
+	inline double GetRadius() { return this->radius; }
+	inline double GetRot() { return this->rotation; }
+
+	inline void SetLength(double radius) { this->radius = radius; }
+	inline void SetRot(double rotation) { this->rotation = rotation; }
+
+	virtual void SetCalculateRadius(double r) { Barrier::SetCalculateRadius(r); }
+	virtual void GetPos(double points[]);
+
+private:
+	double radius;		// The distance between centroid and a vertex
+	double rotation;
+};
+#endif // !BARRIERS_H_
